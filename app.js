18414102ea3c407e45033e3374931b46a7c89d85
jomo85 <praytop@gmail.com>
2023-03-13 22:26:32 +0900

    HTML 기본 속성으로 변경

--- app.js
+++ app.js
@@ -6,12 +6,8 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-  const usename = loginInput.value;
-  if (usename === "") {
-    alert("Please write your name.");
-  } else if (usename.length > 15) {
-    alert("Your name is to long.");
-  }
+  const username = loginInput.value;
+  console.log(username);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
