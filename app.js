7c6385d645170c1dacaa90a03c54e579207ccd23
jomo85 <praytop@gmail.com>
2023-03-14 21:49:42 +0900

    4.4 Getting Username

--- app.js
+++ app.js
@@ -2,15 +2,19 @@
 // .login-form => Class
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 // JavaScript에서 이벤트의 관한 정보를 첫번째 인자로 넘겨준다.
 function onLoginSubmit(event) {
   // 브라우저의 기본동작을 막는 함수
   event.preventDefault();
   const username = loginInput.value;
-  console.log(username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`;
+  //greeting.innerText = "Hello " + username;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function handleLinkClick(event) {
@@ -18,4 +22,3 @@ function handleLinkClick(event) {
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
