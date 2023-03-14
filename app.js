74262b4eefe2557f52526bea45ab33a6cec03817
jomo85 <praytop@gmail.com>
2023-03-14 22:25:01 +0900

    4.6 Loading Username

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 // JavaScript에서 이벤트의 관한 정보를 첫번째 인자로 넘겨준다.
 function onLoginSubmit(event) {
@@ -12,15 +13,24 @@ function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  greeting.innerText = `Hello ${username}`;
-  //greeting.innerText = "Hello " + username;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
   greeting.classList.remove(HIDDEN_CLASSNAME);
-
-  localStorage.setItem("username", username);
+  greeting.innerText = `Hello ${username}`;
 }
 
-function handleLinkClick(event) {
-  event.preventDefault();
-}
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-loginForm.addEventListener("submit", onLoginSubmit);
+// localStorage 의 username 값을 확인.
+if (savedUsername === null) {
+  // show the form
+  // 값이 없는 경우 로그인 폼이 보이고 submit 이벤트 등록
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // show the greeting
+  paintGreetings(savedUsername);
+}
